396d7001eb72b7b969c11bacd436a5a7983c7b6f
DESKTOP-61D76IS\user <forthewy@gmail.com>
2023-04-07 18:47:04 +0900

    [임세혁] -최초커밋

---
+++ src/main/java/com/marondal/choongmotour/ChoongMoTourApplication.java
@@ -0,0 +1,13 @@
+package com.marondal.choongmotour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoongMoTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoongMoTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marondal/choongmotour/ChoongMoTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marondal.choongmotour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoongMoTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
