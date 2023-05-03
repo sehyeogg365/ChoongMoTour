46d6921d589c8e72c0b336f65802564f9ffd8a7d
sehyeogi360 <lsh27572757@gmail.com>
2023-05-03 22:56:04 +0900

    임세혁] -집->학원 매퍼도 뭐 안쓰고 그냥 실행해버리면 실행자체가 안되는오류가 생길수도 있으니 조심하자, 그외에 찜디테일, 로징데티엘 모델 추가

---
+++ src/main/java/com/marondal/choongmotour/lodging/model/LodgingDetail.java
@@ -0,0 +1,11 @@
+package com.marondal.choongmotour.lodging.model;
+
+public class LodgingDetail {//숙소카드 하나에 필요한 데이터들
+	private int id;
+	private String roomName;
+	
+	private String imagePath;
+	private String level;
+	private String areaName;
+
+}
