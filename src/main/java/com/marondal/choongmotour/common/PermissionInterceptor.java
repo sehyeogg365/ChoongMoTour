fafab2126c8522957b149c49c98ac69748da5296
sehyeogi360 <lsh27572757@gmail.com>
2023-05-11 14:47:21 +0900

    [임세혁] -집->학원 퍼미션 인터셉터 모순적인게 많다. 로그인 안됐는데 하굴루의 마이페이지가 조회되는둥 근데 이것도 필연적으로 물어봐야 할듯

--- src/main/java/com/marondal/choongmotour/common/PermissionInterceptor.java
+++ src/main/java/com/marondal/choongmotour/common/PermissionInterceptor.java
@@ -20,6 +20,8 @@ public class PermissionInterceptor implements HandlerInterceptor {
 		HttpSession session = request.getSession();
 		
 		Integer userId = (Integer)session.getAttribute("userId");//이게저장되있으면 로그인 된상태 아니면 로그인 안된상태
+		//Integer adminId = (Integer)session.getAttribute("adminId");
+		
 		//null이저장가능하게 끔 
 		
 		// /user/signin/view
@@ -44,13 +46,12 @@ public class PermissionInterceptor implements HandlerInterceptor {
 			// 리스트, 글쓰기, 상세화면 페이지로 접근하려고 하면
 			// post 로 시작하는 페이지에 접근하려고 하면 	
 			// 로그인 페이지로 이동해라
-				if(uri.startsWith("/user/reservepage")) {//예약페이지
+		
+				if(uri.startsWith("/user")) {//찜목록페이지
 					response.sendRedirect("/user/signin/view");
 					return false;
 				}
-				
-				
-				if(uri.startsWith("/user/dibspage")) {//찜목록페이지
+				if(uri.startsWith("/lodging/dibspage")) {//찜목록페이지
 					response.sendRedirect("/user/signin/view");
 					return false;
 				}
@@ -66,6 +67,42 @@ public class PermissionInterceptor implements HandlerInterceptor {
 				}
 				
 			}
+		
+		
+//		if(adminId != null) {
+//			// 로그인이 되었을때
+//			// 회원 가입, 로그인 페이지 접근하려고 하면
+//			// user 로 시작하는 페이지에 접근하려고 하면 
+//			// 리스트 페이지로 이동시켜라
+//
+//			if(uri.startsWith("/admin")) {//admin 라는 시작하는걸로 접근할려면?
+//				// 리다이렉트 
+//				response.sendRedirect("/admin/main/view");//이동하고픈 주소
+//				//다른페이지로 이동하는거를 대신 try catch 대신 throw
+//				
+//				return false;//원래 가려 했던 그 목적페이지에 못가게 하려면 폴스
+//			}
+//			
+//			} else {
+//			// 로그인이 안되었을떄
+//			// 리스트, 글쓰기, 상세화면 페이지로 접근하려고 하면
+//			// admin 로 시작하는 페이지에 접근하려고 하면 	
+//			// 로그인 페이지로 이동해라
+//				if(uri.startsWith("/admin/lodging")) {//숙소 추가, 숙소 수정
+//					response.sendRedirect("/admin/signin/view");
+//					return false;
+//				}
+//				
+//				
+//				if(uri.startsWith("/admin/room")) {//객실 추가, 객실 수정
+//					response.sendRedirect("/admin/signin/view");
+//					return false;
+//				}
+//	
+//			}
+		
+		
+		
 		
 		
 		return true;//문제없이 여기까지 왔으면 정상수행
