3ef7d673de0516d794a86e7cf5eecd8161e901f9
DESKTOP-61D76IS\user <forthewy@gmail.com>
2023-05-16 17:31:45 +0900

    [임세혁] - 학원->집 빌드 그래들, 파일매니저, 등등 배포에 필요한 코드, 파일만 커밋할것. 그외엔 커밋하지 말것. 코드 수정은 develop에서

--- src/main/java/com/marondal/choongmotour/ChoongMoTourApplication.java
+++ src/main/java/com/marondal/choongmotour/ChoongMoTourApplication.java
@@ -2,9 +2,10 @@ package com.marondal.choongmotour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChoongMoTourApplication {
+public class ChoongMoTourApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChoongMoTourApplication.class, args);
