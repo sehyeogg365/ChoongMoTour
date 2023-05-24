34155682ba0eb43791d1c18f1fb76314dcd81962
sehyeogi360 <lsh27572757@gmail.com>
2023-05-24 11:51:01 +0900

    [임세혁] -컨트롤러, 비오, 다오, 매퍼

--- src/main/java/com/marondal/choongmotour/lodging/comment/bo/CommentBO.java
+++ src/main/java/com/marondal/choongmotour/lodging/comment/bo/CommentBO.java
@@ -1,5 +1,37 @@
 package com.marondal.choongmotour.lodging.comment.bo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.marondal.choongmotour.common.FileManagerService;
+import com.marondal.choongmotour.lodging.bo.LodgingBO;
+import com.marondal.choongmotour.lodging.comment.dao.CommentDAO;
+
+@Service
 public class CommentBO {
+	
+	@Autowired
+	private LodgingBO lodgingBO;
+	
+	@Autowired
+	private CommentDAO commentDAO;
+	
+	public int addComment(int roomId, int userId, MultipartFile file, String content, double starpoint) {
+		
+		String imagePath = FileManagerService.saveFile(userId, file);
+		
+		return commentDAO.insertComment(roomId, userId, imagePath, content, starpoint);
+		
+		
+	}
+	
+
+	
+	
+	
+	
+	
+	
 
 }
--- src/main/java/com/marondal/choongmotour/lodging/comment/dao/CommentDAO.java
+++ src/main/java/com/marondal/choongmotour/lodging/comment/dao/CommentDAO.java
@@ -1,5 +1,21 @@
 package com.marondal.choongmotour.lodging.comment.dao;
 
-public class CommentDAO {
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface CommentDAO {
+	
+	//댓글 추가
+	public int insertComment(@Param("roomId") int roomId
+							, @Param("userId") int userId
+							, @Param("imagePath") String imagePath
+							, @Param("content") String content
+							, @Param("starpoint") double starpoint
+					);
+	
+	
+	//댓글 리스트
+	
+	//댓글 삭제
 }
