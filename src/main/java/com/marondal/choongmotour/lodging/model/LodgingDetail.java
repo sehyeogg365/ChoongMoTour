e5f937eb55b6503ed274f91af4cdc4c7f46940a1
sehyeogi360 <lsh27572757@gmail.com>
2024-01-06 20:49:50 +0900

    [임세혁] -퍼미션 인터셉터 댓글작성 창 추가, dto 주석추가

--- src/main/java/com/marondal/choongmotour/lodging/model/LodgingDetail.java
+++ src/main/java/com/marondal/choongmotour/lodging/model/LodgingDetail.java
@@ -6,7 +6,7 @@ public class LodgingDetail {//숙소카드 하나에 필요한 데이터들
 	private boolean isDibs;//찜했는지 안했는지 여부	
 	private String level;
 	private String areaName;
-	private String imagePath;
+	private String imagePath;//여기에 댓글개수, 평균평점도 추가
 	public int getId() {
 		return id;
 	}
