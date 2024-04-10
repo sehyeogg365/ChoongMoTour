2ff5d90c534070be32ea6107d0ef66614d842472
sehyeogi360 <lsh27572757@gmail.com>
2024-04-10 10:37:18 +0900

    [임세혁] -페이지네이션 관련 변수들 추가

--- src/main/java/com/marondal/choongmotour/lodging/model/CommentDetail.java
+++ src/main/java/com/marondal/choongmotour/lodging/model/CommentDetail.java
@@ -13,6 +13,16 @@ public class CommentDetail {
 	private String imagePath;
 	private double starpoint;	
 	private Date createdAt;
+
+	//페이지네이션 관련 변수들 추가
+	/*
+	private Integer SIZE;
+	private Integer PAGE;				//현재페이지번호
+	private Integer COUNTLIST;			//한 페이지에 출력될 게시물 수 (
+	private Integer BEGIN;
+	private Integer TOTALDATA;
+	* */
+
 	public int getId() {
 		return id;
 	}
